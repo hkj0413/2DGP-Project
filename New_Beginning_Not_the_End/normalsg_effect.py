3f77bb46a71d036e58bca8d20e6e007b0949c1fd
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-11-27 18:19:42 +0900

    이펙트 이미지 최적화

--- New_Beginning_Not_the_End/normalsg_effect.py
+++ New_Beginning_Not_the_End/normalsg_effect.py
@@ -11,23 +11,21 @@ class NormalSGEffcet:
         self.x = server.character.x
         self.y = server.character.y
         self.sx = 0
-        self.framex = 0
+        self.frame = 0
         self.face = d
         if NormalSGEffcet.image == None:
-            NormalSGEffcet.image = load_image("./Effect/" + 'Lc_SG' + ".png")
+            NormalSGEffcet.image = [load_image("./Effect/SG/" + 'Lc_SG' + " (%d)" % i + ".png") for i in range(1, 9 + 1)]
 
     def update(self):
         self.sx = self.x - server.background.window_left
-        self.framex = self.framex + 9.0 * 1.5 * game_framework.frame_time
-        if self.framex > 8.0:
+        self.frame = self.frame + 9.0 * 1.5 * game_framework.frame_time
+        if self.frame > 8.0:
             game_world.remove_object(self)
 
     def draw(self):
         if -15 <= self.sx <= 1095:
-            if self.framex <= 8.0:
+            if self.frame <= 8.0:
                 if self.face == 1:
-                    self.image.clip_composite_draw(int(self.framex) * 155, 0, 155, 157, 0, '',
-                                                   self.sx + 60 + int(self.framex) * 10, self.y - 17, 155, 157)
+                    self.image[int(self.frame)].composite_draw(0, '', self.sx + 60 + int(self.frame) * 10, self.y - 17, 155, 157)
                 elif self.face == -1:
-                    self.image.clip_composite_draw(int(self.framex) * 155, 0, 155, 157, 0, 'h',
-                                                   self.sx - 60 - int(self.framex) * 10, self.y - 17, 155, 157)
+                    self.image[int(self.frame)].composite_draw(0, 'h', self.sx - 60 - int(self.frame) * 10, self.y - 17, 155, 157)
