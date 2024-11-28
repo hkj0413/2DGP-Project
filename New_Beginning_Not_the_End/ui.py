9ca46ecad69db7354625a0814a621552a23b43e1
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-11-28 15:16:47 +0900

    스킬 쿨타임 아이콘으로 표시

--- New_Beginning_Not_the_End/ui.py
+++ New_Beginning_Not_the_End/ui.py
@@ -7,12 +7,21 @@ import character
 class UI:
     image_hp = None
     image_bullet = None
+    image_dash = None
+    image_rc_sg = None
+    image_rc_hg = None
 
     def __init__(self):
         if UI.image_hp == None:
             self.hp_image = load_image('Hp.png')
         if UI.image_bullet == None:
             self.bullet_image = load_image('Bullet.png')
+        if UI.image_dash == None:
+            self.image_dash = load_image("./Icon/" + 'All_dash' + ".png")
+        if UI.image_rc_sg == None:
+            self.image_rc_sg = load_image("./Icon/" + 'SG_defensive_stance' + ".png")
+        if UI.image_rc_hg == None:
+            self.image_rc_hg = load_image("./Icon/" + 'HG_agile_shooting' + ".png")
         self.font = load_font('ENCR10B.TTF', 20)
 
     def update(self):
@@ -36,15 +45,11 @@ class UI:
 
         self.font.draw(560, 780.0, f'Score : {Character.score}', (0, 0, 0))
 
-        if Character.stance == 0:
-            self.font.draw(80, 40.0, 'Dash ', (15, 15, 255))
-            if Character.dash_cooldown == 0:
-                self.font.draw(140, 40.0, 'on', (15, 255, 15))
-            elif not Character.dash_cooldown == 0:
-                self.font.draw(140, 40.0, 'off', (255, 15, 15))
+        if Character.dash_cooldown == 0:
+            self.image_dash.draw(124, 40, 48 ,48)
 
-            self.font.draw(210, 40.0, 'Rc ', (15, 15, 255))
-            self.font.draw(250, 40.0, 'on', (15, 255, 15))
+        if Character.stance == 0:
+            self.image_rc_sg.draw(124 + 64 * 3, 40, 48 ,48)
 
             for i in range(8):
                 if i < Character.bullet_SG:
@@ -55,11 +60,7 @@ class UI:
                 for i in range(Character.shield_def):
                     self.bullet_image.clip_composite_draw(162, 0, 54, 50, 0, '', bx - i * 27 + 2, by - 40, 32, 30)
         elif Character.stance == 1:
-            self.font.draw(80, 40.0, 'Dash ', (215, 15, 215))
-            if Character.dash_cooldown == 0:
-                self.font.draw(140, 40.0, 'on', (15, 255, 15))
-            elif not Character.dash_cooldown == 0:
-                self.font.draw(140, 40.0, 'off', (255, 15, 15))
+
 
             for i in range(4):
                 if i < Character.bullet_RF:
@@ -70,11 +71,8 @@ class UI:
                 #for i in range(snipe_bullet):
                     #self.Bullet_image.clip_composite_draw(216, 0, 54, 50, 0, '', bx - i * 39 + 11, by - 55, 69, 60)
         elif Character.stance == 2:
-            self.font.draw(80, 40.0, 'Dash ', (215, 115, 15))
-            if Character.dash_cooldown == 0:
-                self.font.draw(140, 40.0, 'on', (15, 255, 15))
-            elif not Character.dash_cooldown == 0:
-                self.font.draw(140, 40.0, 'off', (255, 15, 15))
+            if Character.agile_shooting_cooldown == 0:
+                self.image_rc_hg.draw(124 + 64 * 3, 40, 48 ,48)
 
             for i in range(Character.max_bullet_HG):
                 if i < Character.bullet_HG:
