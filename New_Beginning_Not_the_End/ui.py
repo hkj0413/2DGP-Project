920113622426723915b4924e3db64abe7ed160ec
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-11-28 22:15:57 +0900

    UI 이미지 최적화 및 스코어 비활성화

--- New_Beginning_Not_the_End/ui.py
+++ New_Beginning_Not_the_End/ui.py
@@ -15,14 +15,14 @@ class UI:
         if UI.image_hp == None:
             self.hp_image = [load_image("./Icon/" + 'Heart' + " (%d)" % i + ".png") for i in range(1, 3 + 1)]
         if UI.image_bullet == None:
-            self.bullet_image = load_image('Bullet.png')
+            self.bullet_image = [load_image("./Icon/" + 'Bullet' + " (%d)" % i + ".png") for i in range(1, 8 + 1)]
         if UI.image_dash == None:
             self.image_dash = load_image("./Icon/" + 'All_dash' + ".png")
         if UI.image_rc_sg == None:
             self.image_rc_sg = load_image("./Icon/" + 'SG_defensive_stance' + ".png")
         if UI.image_rc_hg == None:
             self.image_rc_hg = load_image("./Icon/" + 'HG_agile_shooting' + ".png")
-        self.font = load_font('ENCR10B.TTF', 20)
+        #self.font = load_font('ENCR10B.TTF', 20)
 
     def update(self):
         pass
@@ -43,7 +43,7 @@ class UI:
         bx = 1060
         by = 770
 
-        self.font.draw(560, 780.0, f'Score : {Character.score}', (0, 0, 0))
+        #self.font.draw(560, 780.0, f'Score : {Character.score}', (0, 0, 0))
 
         if Character.dash_cooldown == 0:
             self.image_dash.draw(124, 40, 48 ,48)
@@ -53,20 +53,20 @@ class UI:
 
             for i in range(8):
                 if i < Character.bullet_SG:
-                    self.bullet_image.clip_composite_draw(0, 0, 27, 50, 0, '', bx - i * 27, by, 27, 50)
+                    self.bullet_image[0].draw(bx - i * 27, by, 27, 50)
                 else:
-                    self.bullet_image.clip_composite_draw(27, 0, 27, 50, 0, '', bx - i * 27, by, 27, 50)
+                    self.bullet_image[1].draw(bx - i * 27, by, 27, 50)
             if Character.state == 1 or character.Reload_SG:
                 for i in range(Character.shield_def):
-                    self.bullet_image.clip_composite_draw(162, 0, 54, 50, 0, '', bx - i * 27 + 2, by - 40, 32, 30)
+                    self.bullet_image[6].draw(bx - i * 27, by - 40, 25, 30)
         elif Character.stance == 1:
 
 
             for i in range(4):
                 if i < Character.bullet_RF:
-                    self.bullet_image.clip_composite_draw(54, 0, 27, 50, 0, '', bx - i * 27, by, 27, 50)
+                    self.bullet_image[2].draw(bx - i * 27, by)
                 else:
-                    self.bullet_image.clip_composite_draw(81, 0, 27, 50, 0, '', bx - i * 27, by, 27, 50)
+                    self.bullet_image[3].draw(bx - i * 27, by)
             #if state == 1:
                 #for i in range(snipe_bullet):
                     #self.Bullet_image.clip_composite_draw(216, 0, 54, 50, 0, '', bx - i * 39 + 11, by - 55, 69, 60)
@@ -77,19 +77,15 @@ class UI:
             for i in range(Character.max_bullet_HG):
                 if i < Character.bullet_HG:
                     if i >= 20:
-                        self.bullet_image.clip_composite_draw(108, 0, 27, 50, 0, '', bx - (i - 20) * 28, by - 56,
-                                                              27, 50)
+                        self.bullet_image[4].draw(bx - (i - 20) * 27, by - 44)
                     elif i >= 10:
-                        self.bullet_image.clip_composite_draw(108, 0, 27, 50, 0, '', bx - (i - 10) * 28, by - 28,
-                                                              27, 50)
+                        self.bullet_image[4].draw(bx - (i - 10) * 27, by - 17)
                     else:
-                        self.bullet_image.clip_composite_draw(108, 0, 27, 50, 0, '', bx - i * 28, by, 27, 50)
+                        self.bullet_image[4].draw(bx - i * 27, by + 10)
                 else:
                     if i >= 20:
-                        self.bullet_image.clip_composite_draw(136, 0, 27, 50, 0, '', bx - (i - 20) * 28, by - 56,
-                                                              27, 50)
+                        self.bullet_image[5].draw(bx - (i - 20) * 27, by - 44)
                     elif i >= 10:
-                        self.bullet_image.clip_composite_draw(136, 0, 27, 50, 0, '', bx - (i - 10) * 28, by - 28,
-                                                              27, 50)
+                        self.bullet_image[5].draw(bx - (i - 10) * 27, by - 17)
                     else:
-                        self.bullet_image.clip_composite_draw(136, 0, 27, 50, 0, '', bx - i * 28, by, 27, 50)
+                        self.bullet_image[5].draw(bx - i * 27, by + 10)
