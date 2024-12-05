5b1fbf90b09730587a6eeace33c2c3414a47df06
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-12-06 04:40:02 +0900

    포탈 넘기 사운드 추가

--- portal.py
+++ portal.py
@@ -1,17 +1,22 @@
 import server
 import character
 
-from pico2d import load_image, draw_rectangle
+from pico2d import load_image, draw_rectangle, load_wav
 
 class Portal:
     image = None
+    sound = None
 
     def __init__(self, i=0.0, j=0.0):
         self.x = i * 30.0 + 15.0
         self.y = j * 30.0 + 15.0
         self.sx = 0
+        self.one = 0
         if Portal.image == None:
             Portal.image = load_image("./Block/" + 'Block' + " (15)" + ".png")
+        if Portal.sound == None:
+            Portal.sound = load_wav("./Sound/Portal.mp3")
+            Portal.sound.set_volume(16)
 
     def update(self):
         self.sx = self.x - server.background.window_left
@@ -29,4 +34,7 @@ class Portal:
         return self.sx - 15.0, self.y - 15.0, self.sx + 15.0, self.y + 15.0
 
     def handle_collision(self, group, other):
-        pass
+        if group == 'server.character:portal':
+            if self.one == 0:
+                Portal.sound.play()
+                self.one += 1
